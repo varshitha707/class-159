eedebbbd456efa40828d83ef16c48b93d180c99b
varshitha707 <124167125+varshitha707@users.noreply.github.com>
2023-06-21 18:52:30 +0530

    Add files via upload

---
+++ js/Tour.js
@@ -0,0 +1,102 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "spider-man",
+        title: "Spider Man",
+        url: "assets/thumbnails/Amazing_Spider-Man_Vol_1_546.webp",
+      },
+      {
+        id: "super-man",
+        title: "Super Man",
+        url: "assets/thumbnails/superman.jpg",
+      },
+
+      {
+        id: "captain-aero",
+        title: "Captain Aero",
+        url: "assets/thumbnails/captain_aero.jpg",
+      },
+      {
+        id: "outer-space",
+        title: "Outer Space",
+        url: "assets/thumbnails/outer_space.jpg",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "ring",
+      radiusInner: 9,
+      radiusOuter: 10,
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+
+    //Add cursor-listener component to the ring border entity to change it's color 
+    //On Cursor 'mouseenter' and 'mouseleave' entity
+    entityEl.setAttribute("cursor-listener", {});
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "circle",
+      radius: 9,
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    
+    return entityEl;
+  },
+});
